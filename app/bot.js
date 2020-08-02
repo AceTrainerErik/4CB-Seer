26b29350b86fa074df4147421e05493f740e273d
AceTrainerErik <acetrainererik@gmail.com>
2020-08-02 14:31:48 -0400

    Initial framework setup.

--- app/bot.js
+++ app/bot.js
@@ -1,13 +1,17 @@
+
+// Requiring.
 var Discord = require('discord.js');
 var auth = require('../config/auth.json');
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Logged in as ${client.user.tag}!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
+  console.log(`Channel: ${message.channel.type}`);
+  console.log(`Message: ${message.content}`);
 	if (message.content === '!ping') {
 		message.channel.send('Pong.');
         console.log('Ping received. Pong sent.');
